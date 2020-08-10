caed830d5fe9206e637f593c6194aa3bed54b641
Yue Yang <marco_yue@163.com>
2020-08-10 21:23:24 +0800

    Basic class

--- Py/Reposition.py
+++ Py/Reposition.py
@@ -5,6 +5,7 @@ from math import radians, cos, sin, asin, sqrt
 import random
 import Graph
 from Graph import Graph
+import pulp
 
 class Reposition(object):
     
@@ -37,6 +38,50 @@ class Reposition(object):
             Repositioning_action[driver_id]=a
             
         return Repositioning_action
+
+    def MILP_Optimization(self,Driver_list,Destination_list,Cost_,Capacity_):
+
+
+        '''Define the problem'''
+
+        model = pulp.LpProblem("Resource constrained assignment problem", pulp.LpMinimize)
+
+        '''Construct our decision variable lists'''
+
+        X = pulp.LpVariable.dicts("X",((i, j) for i in Driver_list for j in Destination_list),lowBound=0,upBound=1,cat='Integer')
+
+        '''Objective Function'''
+
+        model += (pulp.lpSum([-1 * Cost_[j] * X[(i, j)] for i in Driver_list for j in Destination_list]))
+
+
+
+        '''Each drive can only be assigened one destination'''
+
+        for i in Driver_list:
+
+            model += pulp.lpSum([X[(i, j)] for j in Destination_list]) <=1
+            
+        '''Resource Constraints'''
+
+        for j in Destination_list:
+            
+             model += pulp.lpSum([X[(i, j)] for i in Driver_list]) <=Capacity_[j]
+            
+
+
+        model.solve()
+
+
+        reposition_action={}
+
+        for var in X:
+            
+            var_value = X[var].varValue
+            if var_value !=0:
+                reposition_action[var[0]]=var[1]
+            
+        return reposition_action
 
     def MCMF_reposition(self,Driver_list,Destination_list,Destination_Space,Cost_dic,Capacity_dic):
 
@@ -105,13 +150,13 @@ class Reposition(object):
 
             cost[0][driver_idx]=0.0
 
-            for dest in Destination_Space[driver]:
+            for dest in Cost_dic[driver].keys():
 
                 dest_idx=Destination_dic[dest]
 
                 capacity[driver_idx][dest_idx]=1.0
 
-                cost[driver_idx][dest_idx]=-1*Cost_dic[driver][dest]
+                cost[driver_idx][dest_idx]=-1*Cost_dic[dest]
 
                 capacity[dest_idx][-1]=Capacity_dic[dest]
 
---
+++ Py/Stamp_transition.py
@@ -0,0 +1,42 @@
+import time
+import datetime
+import pandas as pd
+
+class Stamp_transition(object):
+    
+    def __init__(self, **kwargs):
+        """ Load your trained model and initialize the parameters """
+        pass
+    
+    def Get_date(self,stamp):
+        dateArray = datetime.datetime.fromtimestamp(stamp)
+        otherStyleTime = dateArray.strftime("%Y-%m-%d %H:%M:%S")
+        return otherStyleTime[:10]
+    
+    '''Time stamp'''
+    def Get_stamp(self,time_str):
+        timeArray = time.strptime(time_str, "%Y-%m-%d %H:%M:%S")
+        timeStamp = int(time.mktime(timeArray))
+        return timeStamp
+    
+    '''Time step'''
+    def Get_step(self,stamp,date_str,step):
+        baseline = date_str+" 00:00:00";
+        baseline = int(self.Get_stamp(baseline))
+        current_step=int((stamp-baseline)/step)
+        return current_step
+    
+    def Get_datelist(self, beginDate, endDate):
+        date_list=[datetime.datetime.strftime(x,'%Y-%m-%d') for x in list(pd.date_range(start=beginDate, end=endDate))]
+        return date_list
+    
+    def Get_weekday(self,date_str):
+        date_str = date_str+" 00:00:00";
+        date_str = time.strptime(date_str, "%Y-%m-%d %H:%M:%S")
+        return date_str.tm_wday+1
+
+    def Get_normalization(self,end_step,t_step):
+        if t_step>=end_step:
+            return t_step-end_step
+        else:
+            return t_step
